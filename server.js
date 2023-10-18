426c1aaf9d243074a2e27f50f48dbf4cad537363
rukia assefa <rukinaassefa@gmail.com>
2023-10-17 20:43:07 -0400

    updated script and port

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ require("dotenv").config();
 const userRouter = require("./routes/user");
 
 
-const PORT = 3000; 
+const PORT = process.env.PORT || 3000; 
 app.listen(PORT, () => {
   console.log(`Server is running in localhost:${PORT}`);
 });
